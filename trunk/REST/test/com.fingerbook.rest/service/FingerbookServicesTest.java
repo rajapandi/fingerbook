04b7e1ee31cd4f1a892f8104336c6840ea29d1e4
sima.cobo <sima.cobo@e81b2268-02d1-3530-7286-21b0b4559b63>
2010-04-16 23:45:52 +0000

    changing tests to com.fingerbook.rest

---
+++ trunk/REST/test/com.fingerbook.rest/service/FingerbookServicesTest.java
@@ -0,0 +1,32 @@
+package fingerbook.service;
+
+import fingerbook.domain.Fingerbook;
+import junit.framework.TestCase;
+
+public class FingerbookServicesTest extends TestCase {
+
+	public void testGetFingerbook() throws Exception {
+		FingerbookServices controller = new FingerbookServices();
+		Fingerbook fingerbook = controller.getFingerbook(new Long(1));
+		if (fingerbook != null) {
+			assertNotNull(fingerbook.getFingerbookId());
+			assertNotNull(fingerbook.getFingerPrints());
+			assertNotNull(fingerbook.getStamp());
+			assertNotNull(fingerbook.getUserInfo());
+			assertEquals(new Long(1), fingerbook.getFingerbookId());
+		}
+	}
+	
+	public void testEmptyGetFingerbook() throws Exception {
+		FingerbookServices controller = new FingerbookServices();
+		assertNull(controller.getFingerbook(new Long(-1)));
+	}
+	
+	public void testGetFingerprintsWithHash() throws Exception {
+	}
+	
+	public void testEmptyGetFingerprintsWithHash() throws Exception {
+		FingerbookServices controller = new FingerbookServices();
+		assertNull(controller.getFingerprintsWithHash(null));
+	}
+}
