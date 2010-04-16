d69ad47f0f24b5314f5caad52d181f962964a227
sima.cobo <sima.cobo@e81b2268-02d1-3530-7286-21b0b4559b63>
2010-04-16 23:45:52 +0000

    changing tests to com.fingerbook.rest

---
+++ REST/test/com.fingerbook.rest/domain/FingerprintsTest.java
@@ -0,0 +1,7 @@
+package fingerbook.domain;
+
+import junit.framework.TestCase;
+
+public class FingerprintsTest extends TestCase {
+
+}
---
+++ REST/test/com.fingerbook.rest/web/FingerbooksControllerTest.java
@@ -0,0 +1,8 @@
+package fingerbook.web;
+
+import junit.framework.TestCase;
+
+public class FingerbooksControllerTest extends TestCase {
+	
+	//TODO: Test Controller with Spring REST Templates
+}
