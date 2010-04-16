5fb5c55babe88e5438e3af9e925828ebb73b2ce9
sima.cobo <sima.cobo@e81b2268-02d1-3530-7286-21b0b4559b63>
2010-04-16 20:21:50 +0000

    New tests written for FingerbookServices.java FingerPrintsController added IoC and page not found handler missing

--- trunk/REST/test/fingerbook/web/FingerbooksControllerTest.java
+++ trunk/REST/test/fingerbook/web/FingerbooksControllerTest.java
@@ -1,11 +1,8 @@
 package fingerbook.web;
 
-import org.springframework.web.servlet.ModelAndView;
-
 import junit.framework.TestCase;
 
 public class FingerbooksControllerTest extends TestCase {
-
-    public void testHandleRequestView() throws Exception{		
-    }
+	
+	//TODO: Test Controller with Spring REST Templates
 }
