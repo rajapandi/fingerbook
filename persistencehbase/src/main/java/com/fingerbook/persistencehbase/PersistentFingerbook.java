7a8261d1d60d7b6478582a970c05d821c799d2ca
onahuel <onahuel@e81b2268-02d1-3530-7286-21b0b4559b63>
2010-04-30 10:34:34 +0000

    Load fingerbook with hashes and save stamp

--- persistencehbase/src/main/java/com/fingerbook/persistencehbase/PersistentFingerbook.java
+++ persistencehbase/src/main/java/com/fingerbook/persistencehbase/PersistentFingerbook.java
@@ -25,6 +25,8 @@ public class PersistentFingerbook extends Fingerbook{
 	public static String TFINGER_GROUP_FAMILY = "group";
 	public static String TGROUP_FINGER_FAMILY = "finger";
 	public static String TGROUP_INFO_FAMILY = "info";
+	
+	public static String COLUMN_STAMP = "stamp";
 
 	public PersistentFingerbook(Fingerbook fingerBook) {
 		this.fingerbookId = fingerBook.getFingerbookId();
@@ -45,6 +47,15 @@ public class PersistentFingerbook extends Fingerbook{
 		byte[] groupIdB = Bytes.toBytes(fingerbookId);
 		byte[] stampB = Bytes.toBytes(stamp);
 		
+		try {
+			/* Insert stamp in group table */
+			HbaseManager.putValue(GROUP_TABLE_NAME, groupIdB, TGROUP_INFO_FAMILY, Bytes.toBytes(COLUMN_STAMP), stampB);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return -1;
+		}
+		
 		List<FileInfo> files = fingerPrints.getFiles();
 		for(FileInfo fileInfo: files) {
 			
@@ -52,7 +63,9 @@ public class PersistentFingerbook extends Fingerbook{
 			byte[] shaHashB = Bytes.toBytes(shaHash);
 			
 			try {
+				/* Insert groupId and stamp in finger table */
 				HbaseManager.putValue(FINGER_TABLE_NAME, shaHashB, TFINGER_GROUP_FAMILY, groupIdB, stampB);
+				/* Insert hash in group table */
 				HbaseManager.putValue(GROUP_TABLE_NAME, groupIdB, TGROUP_FINGER_FAMILY, shaHashB, Bytes.toBytes(""));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -70,10 +83,13 @@ public class PersistentFingerbook extends Fingerbook{
 		
 		Fingerprints auxFingerPrints = new Fingerprints();
 		List<FileInfo> auxFiles = new ArrayList<FileInfo>();
-		
+		byte[] fingerbookIdB = Bytes.toBytes(fingerbookId);
 		
 		NavigableMap<byte[],byte[]> familyMap = null;
 		try {
+			byte[] stampB = HbaseManager.getValue(GROUP_TABLE_NAME, fingerbookIdB, TGROUP_INFO_FAMILY, Bytes.toBytes(COLUMN_STAMP));
+			fingerBook.setStamp(Bytes.toLong(stampB));
+			
 			familyMap = HbaseManager.getMembersMap(GROUP_TABLE_NAME, Bytes.toBytes(fingerbookId), TGROUP_FINGER_FAMILY);
 			for(byte[] shaHashB: familyMap.keySet()) {
 				
@@ -94,12 +110,43 @@ public class PersistentFingerbook extends Fingerbook{
 		return fingerBook;
 	}
 	
+	public static Fingerprints loadFingerPrintsByFingerBook(long fingerbookId) {
+		
+		Fingerprints auxFingerPrints = new Fingerprints();
+		List<FileInfo> auxFiles = new ArrayList<FileInfo>();
+		
+		NavigableMap<byte[],byte[]> familyMap = null;
+		try {
+			familyMap = HbaseManager.getMembersMap(GROUP_TABLE_NAME, Bytes.toBytes(fingerbookId), TGROUP_FINGER_FAMILY);
+			if(familyMap != null) {
+				for(byte[] shaHashB: familyMap.keySet()) {
+					
+					String shaHash = Bytes.toString(shaHashB); 
+					
+					FileInfo auxFileInfo = new FileInfo();
+					auxFileInfo.setShaHash(shaHash);
+					auxFiles.add(auxFileInfo);
+				}
+			}
+			auxFingerPrints.setFiles(auxFiles);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return auxFingerPrints;
+	}
+	
 	public static Vector<Fingerbook> getFingerbookByHash(String hash) {
 		
 		Vector<Fingerbook> fingerbooks = new Vector<Fingerbook>();
 		NavigableMap<byte[],byte[]> familyMap = null;
 		try {
 			familyMap = HbaseManager.getMembersMap(FINGER_TABLE_NAME, Bytes.toBytes(hash), TFINGER_GROUP_FAMILY);
+			if(familyMap == null) {
+				return fingerbooks;
+			}
 			for(byte[] fingerbookIdB: familyMap.keySet()) {
 				
 				long fingerbookId = Bytes.toLong(fingerbookIdB);
@@ -108,6 +155,9 @@ public class PersistentFingerbook extends Fingerbook{
 				Fingerbook auxFingerbook = new Fingerbook();
 				auxFingerbook.setFingerbookId(fingerbookId);
 				auxFingerbook.setStamp(stamp);
+				
+				Fingerprints auxFingerPrints = loadFingerPrintsByFingerBook(fingerbookId);
+				auxFingerbook.setFingerPrints(auxFingerPrints);
 				
 				fingerbooks.add(auxFingerbook);
 			}
