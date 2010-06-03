bb9d96c8a05c9b9e9663137238936924f3a36f99
jpampliega <jpampliega@e81b2268-02d1-3530-7286-21b0b4559b63>
2010-06-03 03:53:53 +0000

    Added ticket sending for semi-autenticated posting. Corrected some warnings.

--- Client/src/main/java/com/fingerbook/client/ResponseException.java
+++ Client/src/main/java/com/fingerbook/client/ResponseException.java
@@ -4,6 +4,8 @@ import com.fingerbook.models.Response;
 
 public class ResponseException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+	
 	private Response response;
 	
 	public ResponseException(String msg, Response resp) {
