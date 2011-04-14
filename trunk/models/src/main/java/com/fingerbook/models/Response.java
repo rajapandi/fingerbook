a5f45c0f067b3e5edec19ab4147dcb9d272c7d20
sima.cobo <sima.cobo@e81b2268-02d1-3530-7286-21b0b4559b63>
2011-04-14 02:07:54 +0000

    Implemented communication protocol (resume, authenticated, semi authenticated and anonymous).

--- trunk/models/src/main/java/com/fingerbook/models/Response.java
+++ trunk/models/src/main/java/com/fingerbook/models/Response.java
@@ -4,12 +4,17 @@ public class Response {
 
 	/*
 	 * Error responses from server:
-	 * 		1 - Fingerbooks: Missing arguments
-	 * 		2 - Transaction with fingerbook id: xx could not be finished
-	 * 		3 - Transaction: There was an unexpected error: Transaction start aborted
-	 * 		4 - Invalid fingerbook state
-	 * 		5 - Fingerbook could not be added to fingerbook id: xx
-	 * 		6 - Unimplemented method
+	 * 		1  -  Fingerbooks: Missing arguments
+	 * 		2  -  Transaction with fingerbook id: xx could not be finished
+	 * 		3  -  Transaction: There was an unexpected error: Transaction start aborted
+	 * 		4  -  Invalid fingerbook state
+	 * 		5  -  Fingerbook could not be added to fingerbook id: xx
+	 * 		6  -  Unimplemented method
+	 * 		7  -  Resume cancelled, no transaction ID was received
+	 * 		8  -  Operation cancelled: Anonymous authentication is used, but a ticket was received
+	 * 		9  -  Operation cancelled: Aonymous authentication is used, but a user name was received
+	 * 		10 -  Operation cancelled: Semi-authentication is used, but no ticket was received
+	 * 		11 -  Operation cancelled: Authentication is used, but no user name was received
 	 * 		null - Success!
 	 * */
 
