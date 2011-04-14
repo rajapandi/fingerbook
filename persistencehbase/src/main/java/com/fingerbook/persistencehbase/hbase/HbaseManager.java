79b735af07d2291fa999125aaf7f8e2c31c9599f
onahuel <onahuel@e81b2268-02d1-3530-7286-21b0b4559b63>
2011-04-14 22:33:44 +0000

    Modified TTMP table for saving transactionId

--- persistencehbase/src/main/java/com/fingerbook/persistencehbase/hbase/HbaseManager.java
+++ persistencehbase/src/main/java/com/fingerbook/persistencehbase/hbase/HbaseManager.java
@@ -454,6 +454,58 @@ public class HbaseManager {
 		return ret;
 	}
 	
+	public static Vector<byte[]> getColumnValuesFromFilteredRows(String tableName, byte[] columnFamilyFilter, byte[] columnNameFilter, byte[] value, CompareOp compareOp, byte[] columnFamilyValue, byte[] columnNameValue) throws IOException {
+		
+		Vector<byte[]> ret = new Vector<byte[]>();
+		
+		// You need a configuration object to tell the client where to connect.
+	    // When you create a HBaseConfiguration, it reads in whatever you've set
+	    // into your hbase-site.xml and in hbase-default.xml, as long as these can
+	    // be found on the CLASSPATH
+
+//	    HBaseConfiguration config = new HBaseConfiguration();
+//		Configuration config = HBaseConfiguration.create();
+		
+		if(config == null) {
+			config = HBaseConfiguration.create();
+		}
+	    
+		// This instantiates an HTable object that connects you to
+	    // the "myLittleHBaseTable" table.
+	    HTable table = new HTable(config, tableName);
+	    
+		// Sometimes, you won't know the row you're looking for. In this case, you
+	    // use a Scanner. This will give you cursor-like interface to the contents
+	    // of the table.  To set up a Scanner, do like you did above making a Put
+	    // and a Get, create a Scan.  Adorn it with column names, etc.
+	    Scan s = new Scan();
+	    SingleColumnValueFilter filter = new SingleColumnValueFilter(columnFamilyFilter, columnNameFilter, compareOp, value);
+	    s.setFilter(filter);
+	    
+	    
+	    ResultScanner scanner = table.getScanner(s);
+	    try {
+	      // Scanners return Result instances.
+	      // Now, for the actual iteration. One way is to use a while loop like so:
+	      for (Result rr = scanner.next(); rr != null; rr = scanner.next()) {
+	        // print out the row we found and the columns we were looking for
+//	        System.out.println("Found row: " + rr);
+	        ret.add(rr.getValue(columnFamilyValue, columnNameValue));
+	      }
+	
+	      // The other approach is to use a foreach loop. Scanners are iterable!
+	      // for (Result rr : scanner) {
+	      //   System.out.println("Found row: " + rr);
+	      // }
+	    } finally {
+	      // Make sure you close your scanners when you are done!
+	      // Thats why we have it inside a try/finally clause
+	      scanner.close();
+	    }
+		
+		return ret;
+	}
+	
 	public static Vector<String> filterRows(String tableName, String columnFamily, String columnName, String value) throws IOException {
 		
 		Vector<String> ret = new Vector<String>();
@@ -505,6 +557,8 @@ public class HbaseManager {
 		
 		return ret;
 	}
+	
+	
 	
 	public static void createTable(String tableName, Vector<String> columnFamilies) throws IOException {
 		
