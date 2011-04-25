557c772bf582dcd79d3847820cd13a1077ddc60b
sima.cobo <sima.cobo@e81b2268-02d1-3530-7286-21b0b4559b63>
2011-04-25 23:19:56 +0000

    fbWebClient authenticates users using finbergookRESTM authentication controller (temporarily using GET: .../authenticate/username/password)

---
+++ RESTM/src/main/java/com/fingerbook/rest/web/AuthenticateController.java
@@ -0,0 +1,34 @@
+package com.fingerbook.rest.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fingerbook.models.Response;
+
+@Controller
+@RequestMapping("/authenticate")
+public class AuthenticateController {
+	
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    // TODO: username and password should be sent with POST
+    // TODO: always succesful login
+    @RequestMapping(value="/{username}/{password}", method=RequestMethod.GET)
+    @ResponseBody
+	public Response authenticate(@PathVariable("username") String username, 
+			@PathVariable("password") String password,
+			Model model) {
+    	
+    	String msg = "Authentication succesful for user: " +  username + ", password: " + password;
+    	Response response = new Response(null, msg);
+    	logger.info(msg);
+    	
+		return response;
+	}
+}
--- fbWebClient/src/main/java/com/fingerbook/security/UserAuthenticationProvider.java
+++ fbWebClient/src/main/java/com/fingerbook/security/UserAuthenticationProvider.java
@@ -1,5 +1,9 @@
 package com.fingerbook.security;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +32,7 @@ public class UserAuthenticationProvider extends
 
 	}
 
+	// TODO: Incorrect Exceptions treatment
 	@Override
 	protected UserDetails retrieveUser(String username,
 			UsernamePasswordAuthenticationToken authentication)
@@ -40,13 +45,16 @@ public class UserAuthenticationProvider extends
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         try {
             // TODO: Authenticate using fingerbookRESTM
-        	authenticateAgainstRESTM();
+        	authenticateAgainstRESTM(username, password);
+        	
         	// Speaker speaker = Speaker.findSpeakersByEmailAndPasswordEquals(username, password).getSingleResult();
             authorities.add(new GrantedAuthorityImpl("ROLE_USER"));
         } catch (EmptyResultDataAccessException e) {
             throw new BadCredentialsException("Invalid username or password");
         } catch (NonUniqueResultException e) {
             throw new BadCredentialsException("Non-unique user, contact administrator");
+        } catch (Exception e) {
+        	throw new BadCredentialsException("Invalid username or password");
         }
         return new User(username, password, true, // enabled
                 true, // account not expired
@@ -55,9 +63,27 @@ public class UserAuthenticationProvider extends
                 authorities);
 	}
 
-	private void authenticateAgainstRESTM() throws EmptyResultDataAccessException, BadCredentialsException {
-		// TODO Auto-generated method stub
+	
+	private void authenticateAgainstRESTM(String username, String password) throws EmptyResultDataAccessException, 
+	BadCredentialsException, Exception {
+    		
+		// TODO: Hardwired URL
+		URL url = new URL("http://localhost:8080/fingerbookRESTM/authenticate/"+ username + "/" + password);
+		URLConnection conn = url.openConnection ();
+
+		// Get the response
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		StringBuffer sb = new StringBuffer();
+		String line;
+		// TODO: Use xml marshalling and unmarshalling instead of parsing the lines
+		line = rd.readLine();
 		
+		rd.close();
+		
+		// TODO: Ugly succesful login checking
+		if(!line.contains("Authentication succesful for user")) {
+			throw new BadCredentialsException("Invalid credentials");
+		}
 	}
 
 }
