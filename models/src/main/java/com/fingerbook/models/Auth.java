34a91fb3eadbe6fa477ddb6d7c5f69c2196d6538
onahuel <onahuel@gmail.com>
2012-03-29 07:09:14 +0000

    Fixed static AUTH_ADMIN

--- models/src/main/java/com/fingerbook/models/Auth.java
+++ models/src/main/java/com/fingerbook/models/Auth.java
@@ -5,5 +5,5 @@ public class Auth {
 	public static final int AUTH_ANONYMOUS = 0;
 	public static final int AUTH_SEMI_AUTHENTICATED = 1;
 	public static final int AUTH_AUTHENTICATED = 2;
-	public static final int AUTH_ADMIN = 2;
+	public static final int AUTH_ADMIN = 3;
 }
