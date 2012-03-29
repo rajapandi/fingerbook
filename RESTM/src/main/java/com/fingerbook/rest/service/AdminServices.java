6fae184cfc44a36cb1448ffd5c790949dd1992c0
onahuel <onahuel@e81b2268-02d1-3530-7286-21b0b4559b63>
2012-03-29 07:00:18 +0000

    Branch test 1

--- RESTM/src/main/java/com/fingerbook/rest/service/AdminServices.java
+++
@@ -1,8 +0,0 @@
-package com.fingerbook.rest.service;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public class AdminServices {
-	protected final Log logger = LogFactory.getLog(getClass());
-}
