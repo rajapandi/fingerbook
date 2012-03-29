260feb9e526059fbb215754b9568c1f481379005
onahuel <onahuel@e81b2268-02d1-3530-7286-21b0b4559b63>
2012-03-29 07:15:50 +0000

    Branch 1 Web Client. Backup too many mods

--- RESTM/src/main/java/com/fingerbook/rest/domain/exception/ApplicationException.java
+++
@@ -1,7 +0,0 @@
-package com.fingerbook.rest.domain.exception;
-
-public class ApplicationException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-}
--- models/src/main/java/com/fingerbook/models/Auth.java
+++
@@ -1,9 +0,0 @@
-package com.fingerbook.models;
-
-public class Auth {
-
-	public static final int AUTH_ANONYMOUS = 0;
-	public static final int AUTH_SEMI_AUTHENTICATED = 1;
-	public static final int AUTH_AUTHENTICATED = 2;
-	public static final int AUTH_ADMIN = 3;
-}
