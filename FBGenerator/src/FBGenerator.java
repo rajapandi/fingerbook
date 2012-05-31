365a08cfca5eb5d6749a47f8bc2fb5a1b2830e49
onahuel <onahuel@gmail.com>
2012-05-31 04:19:09 +0000

    Insert directly in Hbase

--- FBGenerator/src/FBGenerator.java
+++ FBGenerator/src/FBGenerator.java
@@ -4,33 +4,54 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 
 import com.fingerbook.models.FileInfo;
 import com.fingerbook.models.Fingerbook;
 import com.fingerbook.models.Fingerbook.STATE;
 import com.fingerbook.models.Fingerprints;
 import com.fingerbook.models.UserInfo;
+import com.fingerbook.persistencehbase.HbaseFBGenerator;
 
 public class FBGenerator {
+	
+	public static Integer topLimit = 100;
+	public static Integer amountOfFingerbook = 2;
+	public static Integer amountOfFingerPrintsLowLimit = 2;
+	public static Integer amountOfFingerPrintsTopLimit = 4;
+	public static String userName = "nahu1";
 
-	static GeneratorParams params = new GeneratorParams();
+//	static GeneratorParams params = new GeneratorParams();
+	static GeneratorParams params;
 	static private UserInfo ui; 
-	static private String userName;
+//	static private String userName;
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		if(getParams(args) == 0) {
-			System.out.println("Invalid parameter");
-		}
-
-		generateHashes();
+		params = new GeneratorParams();
 		
-		for (String s: args) {
-            System.out.println(s);
-        }
+		fillParams();
+		
+//		generateHashes();
+		Vector<Fingerbook> fbs = generateFbs();
+		
+		HbaseFBGenerator.insertFbs(fbs);
+		
+	}
+
+	
+	private static int fillParams() {
+		
+		params.setTopLimit(topLimit);
+		params.setAmountOfFingerbook(amountOfFingerbook);
+		params.setAmountOfFingerPrintsLowLimit(amountOfFingerPrintsLowLimit);
+		params.setAmountOfFingerPrintsTopLimit(amountOfFingerPrintsTopLimit);
+		params.setUserName(userName);
+		
+		return 1;
 	}
 	
 	private static int getParams(String[] args) {
@@ -58,6 +79,53 @@ public class FBGenerator {
 		}
 		
 		return 1;
+	}
+	
+	private static Vector<Fingerbook> generateFbs() {
+		
+		Vector<Fingerbook> fbs = new Vector<Fingerbook>();
+		
+		int amountOfFingerbooks = params.getAmountOfFingerbook();
+		
+		int i=0;
+		for(i=0; i < amountOfFingerbooks; i++) {
+			
+			try {		
+			
+				// Create "FileInfo"s	
+				List<FileInfo> files = createFileInfos();
+
+				
+				Fingerprints fp = new Fingerprints();
+				// Set fingerprint id equal to fingerbook id
+				fp.setFingerprintsId(new Long(i));
+				fp.setFiles(files);
+				
+				Fingerbook fb = new Fingerbook();
+				ui = new UserInfo();
+				ui.setUser(params.getUserName());
+				ui.setTicket(null);
+				
+				fb.setState(STATE.CONTENT);
+				fb.setUserInfo(ui);
+				// Set tags to null
+				fb.setTags(null);
+				fb.setComment("generated_by_script");
+				// Set fingerprint id equal to fingerbook id
+				fb.setFingerbookId(new Long(i));
+				fb.setStamp(new Date().getTime());
+				fb.setFingerPrints(fp);
+			
+				System.out.println(fb.toString());
+				
+				fbs.add(fb);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return fbs;
 	}
 
 	private static void generateHashes() {
@@ -143,5 +211,18 @@ public class FBGenerator {
 		
 		return randomNumber;
 	}
+	
+//	public static void main(String[] args) {
+//	
+//	if(getParams(args) == 0) {
+//		System.out.println("Invalid parameter");
+//	}
+//
+//	generateHashes();
+//	
+//	for (String s: args) {
+//        System.out.println(s);
+//    }
+//}
 
 }
