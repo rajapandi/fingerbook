9a14c90a1f9658ca02ab984aba280dff95823333
onahuel <onahuel@e81b2268-02d1-3530-7286-21b0b4559b63>
2012-08-04 05:17:46 +0000

    Branch 1 mavenRepository. Backup. Adding pig jar

--- fbWebClient/src/main/java/com/fingerbook/security/MyEntryPoint.java
+++
@@ -1,6 +0,0 @@
-package com.fingerbook.security;
-
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-
-public class MyEntryPoint extends LoginUrlAuthenticationEntryPoint {
-}
--- fbWebClient/src/test/java/com/fingerbook/MyClassDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.fingerbook;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = MyClass.class)
-public class MyClassDataOnDemand {
-}
