bb31893be5643af6900c58594b221136dbf53113
germangross <germangross@e81b2268-02d1-3530-7286-21b0b4559b63>
2012-08-19 00:34:59 +0000

    fbClient now works with remote RESTM servers

--- trunk/Client/src/main/java/com/fingerbook/client/HttpClientExt.java
+++ trunk/Client/src/main/java/com/fingerbook/client/HttpClientExt.java
@@ -7,6 +7,6 @@ import org.apache.commons.httpclient.auth.AuthScope;
 public class HttpClientExt extends HttpClient {
 	public void setCredentials(String user, String pass) {
 		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(user, pass);
-		this.getState().setCredentials(new AuthScope("localhost", 8080, AuthScope.ANY_REALM), credentials);
+		this.getState().setCredentials(new AuthScope(AuthScope.ANY), credentials);
 	}
 }
